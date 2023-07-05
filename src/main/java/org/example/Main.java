ce2a159e4d94decc4cb0d288673c94e2e587be63
sebastian1r <127241714+sebastian1r@users.noreply.github.com>
2023-07-05 12:15:48 -0500

    Se crea ejemplo de modificadores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.modelos.Usuario;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Integer edad = 34;
+
+        Universidad cesde = new Universidad("Cesde");
+
+        Usuario usuario = new Usuario();
+
+        System.out.println(cesde.nombre);
+        System.out.println();
     }
 }
---
+++ src/main/java/org/example/Universidad.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Universidad {
+    protected String nombre;
+
+    public Universidad() {
+
+    }
+
+    public Universidad(String nombre) {
+        this.nombre = nombre;
+    }
+}
